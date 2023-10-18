98a8dbb421d00172f4927f85be1899a2a4723cd9
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 12:02:26 +0300

    fix selects in query form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react'
 const SearchForm = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('')
   const [selectedCategory, setSelectedCategory] = useState('all')
+  const [sortingParam, setSortingParam] = useState('relevance')
 
   const handleSearch = () => {
     // Вызов функции onSearch с введенным запросом и выбранной категорией
-    onSearch(searchQuery, selectedCategory)
+    onSearch(searchQuery, selectedCategory, sortingParam)
   }
 
   return (
@@ -21,8 +22,21 @@ const SearchForm = ({ onSearch }) => {
         value={selectedCategory}
         onChange={(e) => setSelectedCategory(e.target.value)}
       >
-        <option value="all">Все категории</option>
+        <option value="all">all</option>
+        <option value="art">art</option>
+        <option value="biography">biography</option>
+        <option value="computers">computers</option>
+        <option value="history">history</option>
+        <option value="medical">medical</option>
+        <option value="all">poetry</option>
         {/* Добавьте другие опции для категорий */}
+      </select>
+      <select
+        value={sortingParam}
+        onChange={(e) => setSortingParam(e.target.value)}
+      >
+        <option value="relevance">relevance</option>
+        <option value="newest">newest</option>
       </select>
       <button onClick={handleSearch}>Поиск</button>
     </div>
