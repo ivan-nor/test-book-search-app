c95a0fe665fab4a0b319ab2a7c9524df34f5ef71
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 18:01:16 +0300

    fix book card tags

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,21 @@
-const Card = ({ book }) => {
+function Card ({ book }) {
+  const { volumeInfo } = book
+  const { categories, authors } = volumeInfo
+
+  // Получаем первую категорию (если она есть)
+  const category = (categories && categories.length) ? `Category: ${categories[0]}` : null
+
+  // Получаем всех авторов (если они есть)
+  const authorList = authors ? `Authors: ${authors.join(', ')}` : null
+
   return (
-    <div>
-      { book.volumeInfo.title } | { book.volumeInfo.categories }
+    <div className="book-card">
+      <img src={volumeInfo.imageLinks?.thumbnail || 'No Image Available'} alt={volumeInfo.title} />
+      <h3>{volumeInfo.title || 'No Title Available'}</h3>
+      <p>{category}</p>
+      <p>{authorList}</p>
     </div>
   )
 }
+
 export default Card
