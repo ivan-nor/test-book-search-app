3117ef150a4eee36a20836d2a2aeadd57f0b7c9f
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 16:29:47 +0300

    fix query params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
+
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import path from 'path'
+// import url from 'url'
+import * as nlb from 'node-libs-browser'
 import './App.css' // Подключаем стили вашего приложения
 import SearchForm from './SearchForm'
+import Card from './Card'
 
 const API_KEY = 'AIzaSyBeJ3IhXXpm4pOFzxclMwqj0PS7n_ZSHdg'
 const API_URL = 'https://www.googleapis.com/books/v1/volumes'
@@ -13,15 +19,21 @@ const App = () => {
   const [books, setBooks] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
 
-  const performSearch = (query, category) => {
+  // const { URLSearchParams } = url
+  // console.log(nlb, url, path)
+  const getCategory = (category) => (category === 'all') ? '' : `+subject:${category}`
+
+  const performSearch = (query, category, sortingParam) => {
     // Параметры запроса
     const params = {
-      q: query, // Поисковый запрос
-      key: API_KEY, // Ваш ключ API,
-      subject: category, // Категория (ваш выбор)
-      orderBy: sortingParam
+      q: `${query}${getCategory(category)}`, // Поисковый запрос
+      // q: query,
+      orderBy: sortingParam,
+      key: API_KEY // Ваш ключ API,
     }
-
+    const searchParams = new URLSearchParams(params)
+    const queryStr = path.join(API_URL, searchParams.toString())
+    console.log(queryStr, params, searchParams.toString())
     // Выполняем GET-запрос к Google Books API
     axios.get(API_URL, { params })
       .then(response => {
@@ -56,6 +68,7 @@ const App = () => {
           {/* Компонент для отображения найденных книг в виде карточек */}
           {/* Компонент кнопки "Load more" для пагинации */}
           { books.length ? `Найдено: ${books.length} книг(а)` : null }
+          { books.map((book) => <Card book={book} key={book.id} />) }
         </div>
       </main>
     </div>
