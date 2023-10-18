35173f153f443524d2b44bb6b107548d5f8dd471
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 18:01:02 +0300

    fix code style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css' // Импортируйте стили Bootstrap
