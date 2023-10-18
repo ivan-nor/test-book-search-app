251ad57c5b71e15f233b11a3b25e07982f51250e
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 16:28:08 +0300

    add modules of core node

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const path = require('path')
+// const url = require('url')
+
+// module.exports = function override (config, env) {
+//   // add your customizations here
+//   config.module.rules.push({
+//     resolve: { fallback: { path, url } }
+//   })
+//   return config
+// }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Импортируйте стили Bootstrap
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css' // Импортируйте стили Bootstrap
 
-import App from './App';
+import App from './App'
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
