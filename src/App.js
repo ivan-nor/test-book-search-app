24e8786f729f8daa8fab160971b758847581d4e4
Ivan Nor <ivan.norik@gmail.com>
2023-10-18 18:01:35 +0300

    fix query params

--- src/App.js
+++ src/App.js
@@ -1,46 +1,40 @@
 /* eslint-disable no-unused-vars */
-
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import path from 'path'
-// import url from 'url'
-import * as nlb from 'node-libs-browser'
-import './App.css' // Подключаем стили вашего приложения
+import './App.css'
 import SearchForm from './SearchForm'
 import Card from './Card'
 
 const API_KEY = 'AIzaSyBeJ3IhXXpm4pOFzxclMwqj0PS7n_ZSHdg'
 const API_URL = 'https://www.googleapis.com/books/v1/volumes'
 
-const App = () => {
+function App () {
   const [searchQuery, setSearchQuery] = useState('')
   const [selectedCategory, setSelectedCategory] = useState('all')
   const [sortingParam, setSortingParam] = useState('relevance')
   const [books, setBooks] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
 
-  // const { URLSearchParams } = url
-  // console.log(nlb, url, path)
   const getCategory = (category) => (category === 'all') ? '' : `+subject:${category}`
 
-  const performSearch = (query, category, sortingParam) => {
+  const performSearch = (query, category, sortingParam) => { // ВЫДЕЛИТЬ В ОТДЕЛЬНЫЙ КОМПОНЕНТ ОТРИСОВКУ РЕЗУЛЬАТТОВ И КНОПКУ ПАГИНАЦИИ
     // Параметры запроса
     const params = {
-      q: `${query}${getCategory(category)}`, // Поисковый запрос
-      // q: query,
+      q: `${query}${getCategory(category)}`,
       orderBy: sortingParam,
-      key: API_KEY // Ваш ключ API,
+      key: API_KEY, // Ваш ключ API,
+      startIndex: 0,
+      maxResults: 30
     }
     const searchParams = new URLSearchParams(params)
-    const queryStr = path.join(API_URL, searchParams.toString())
-    console.log(queryStr, params, searchParams.toString())
+    console.log(params, searchParams.toString())
     // Выполняем GET-запрос к Google Books API
     axios.get(API_URL, { params })
       .then(response => {
         const booksData = response.data.items // Полученные данные о книгах
         // Обработка данных, например, обновление состояния books
         setBooks(booksData)
-        console.log(booksData)
+        console.log(response.data)
       })
       .catch(error => {
         console.error('Ошибка при выполнении запроса к Google Books API', error)
@@ -68,7 +62,11 @@ const App = () => {
           {/* Компонент для отображения найденных книг в виде карточек */}
           {/* Компонент кнопки "Load more" для пагинации */}
           { books.length ? `Найдено: ${books.length} книг(а)` : null }
-          { books.map((book) => <Card book={book} key={book.id} />) }
+          <div className="search-results">
+            {books.map((book) => (
+              <Card key={book.etag} book={book} />
+            ))}
+          </div>
         </div>
       </main>
     </div>
