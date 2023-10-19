f66f178032aeeff986c255860dcffaf93401811e
Ivan Nor <ivan.norik@gmail.com>
2023-10-19 23:28:31 +0300

    set up redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css' // Импортируйте стили Bootstrap
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux' // Импортируйте Provider
+import store from './store' // Импортируйте ваш Redux store
 import App from './App'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}> {/* Оберните компонент App в Provider */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 )
