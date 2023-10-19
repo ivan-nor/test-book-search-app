0f8b9ca7be2be7c36d5aa1b6a03b4978f04e1020
Ivan Nor <ivan.norik@gmail.com>
2023-10-19 23:27:17 +0300

    add config file

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'AIzaSyBeJ3IhXXpm4pOFzxclMwqj0PS7n_ZSHdg'
+export const API_URL = 'https://www.googleapis.com/books/v1/volumes'
+export const PAGINATION_STEP = 30
+export const MAX_RESULTS = 30
