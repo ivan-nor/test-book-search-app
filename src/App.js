83c5916937c92ae0b79fb39acb041fb025a0cf43
Ivan Nor <ivan.norik@gmail.com>
2023-10-19 23:29:24 +0300

    fix state from stateful to redux store

--- src/App.js
+++ src/App.js
@@ -5,53 +5,75 @@ import './App.css'
 import SearchForm from './SearchForm'
 import Card from './Card'
 import SearchResults from './SearchResults'
-
-const API_KEY = 'AIzaSyBeJ3IhXXpm4pOFzxclMwqj0PS7n_ZSHdg'
-const API_URL = 'https://www.googleapis.com/books/v1/volumes'
+import { useDispatch, useSelector } from 'react-redux'
+import { addBooks, updateStartIndex, clearBooks, setIsLoading } from './searchSlice'
+import { API_KEY, API_URL, MAX_RESULTS, PAGINATION_STEP } from './config'
 
 function App () {
-  const [searchQuery, setSearchQuery] = useState('')
-  const [selectedCategory, setSelectedCategory] = useState('all')
-  const [sortingParam, setSortingParam] = useState('relevance')
-  const [books, setBooks] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [step, setStep] = useState(30)
-  const [startIndex, setStartIndex] = useState(0)
   const [countBooks, setCountBooks] = useState(0)
+  const [query, setQuery] = useState('')
+  const [category, setCategory] = useState('all')
+  const [sortingParam, setSortingParam] = useState('relevance')
 
-  const getCategory = (category) => (category === 'all') ? '' : `+subject:${category}`
+  const dispatch = useDispatch()
+  const books = useSelector((state) => state.search.books)
+  const startIndex = useSelector((state) => state.search.startIndex)
+  const isLoading = useSelector((state) => state.search.isLoading)
 
-  const performSearch = (query, category, sortingParam) => { // ВЫДЕЛИТЬ В ОТДЕЛЬНЫЙ КОМПОНЕНТ ОТРИСОВКУ РЕЗУЛЬАТТОВ И КНОПКУ ПАГИНАЦИИ
+  useEffect(() => console.log('UseEffect -> books:', books), [books])
+
+  const performSearch = (query, category, sortingParam, startIndex) => {
+    dispatch(setIsLoading(true))
+
+    const getCategory = (category) => (category === 'all') ? '' : `+subject:${category}`
+
     const params = {
       q: `${query}${getCategory(category)}`,
       orderBy: sortingParam,
       key: API_KEY, // Ваш ключ API,
       startIndex,
-      maxResults: step
+      maxResults: MAX_RESULTS,
+      printType: 'books',
+      filter: 'ebooks'
     }
     const searchParams = new URLSearchParams(params)
     console.log(searchParams.toString(), params)
 
     // Выполняем GET-запрос к Google Books API
     axios.get(API_URL, { params })
-      .then(response => {
+      .then((response) => {
+        console.log(response.data)
         const booksData = response.data.items // Полученные данные о книгах
         setCountBooks(response.data.totalItems)
-        setBooks([...books, ...booksData])
-        console.log(response.data.totalItems)
+        setQuery(query)
+        dispatch(addBooks(booksData))
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Ошибка при выполнении запроса к Google Books API', error)
         // Обработка ошибки
       })
+      .finally(() => {
+        // Сбрасываем состояние загрузки в false после завершения запроса
+        dispatch(setIsLoading(false))
+      })
   }
 
-  // useEffect(() => {}, [books])
+  const handleSearch = (newQuery, newCategory, newSortingParam) => {
+    const newStartIndex = 0
+    setQuery(newQuery)
+    setCategory(newCategory)
+    setSortingParam(newSortingParam)
+    dispatch(updateStartIndex(newStartIndex))
+    dispatch(clearBooks()) // Очистка books
+    console.log('handle Search', newQuery, newCategory, newSortingParam, books)
 
-  const loadMore = () => {
-    console.log('load more')
-    setStartIndex(startIndex + step)
-    performSearch()
+    performSearch(newQuery, newCategory, newSortingParam, newStartIndex)
+  }
+
+  const loadMoreBooks = () => {
+    const newStartIndex = startIndex + PAGINATION_STEP
+    dispatch(updateStartIndex(newStartIndex)) // Обновляем startIndex в хранилище
+    performSearch(query, category, sortingParam, newStartIndex)
   }
 
   return (
@@ -60,10 +82,17 @@ function App () {
         {/* Ваш заголовок или компоненты заголовка здесь */}
       </header>
       <main className="App-main">
-        <SearchForm onSearch={performSearch} /> {/* Передаем функцию поиска в SearchForm */}
+        <SearchForm onSearch={handleSearch} /> {/* Передаем функцию поиска в SearchForm */}
         <div className="book-list">
-          { books.length ? `Найдено: ${countBooks} книг(а)` : null }
-          <SearchResults books={books} loadMore={loadMore} countBooks={countBooks} startIndex={startIndex} />
+          { isLoading
+            ? (<p>Loading...</p>)
+            : (
+                <>
+                  {books.length ? `Найдено: ${countBooks} книг(а), отрисовано: ${books.length}` : null}
+                  <SearchResults books={books} onLoadMore={loadMoreBooks} countBooks={countBooks} startIndex={startIndex} />
+                </>
+              )
+          }
         </div>
       </main>
     </div>
