5164a639b5784041897c756135e887094ba3bfca
Ivan Nor <ivan.norik@gmail.com>
2023-10-19 23:28:01 +0300

    fix prop name

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -1,12 +1,11 @@
+/* eslint-disable no-unused-vars */
 import Card from './Card'
 
-function SearchResults ({ books, loadMore, countBooks, startIndex }) {
-  console.log('Search results', books, loadMore, countBooks, startIndex)
-
+function SearchResults ({ books, onLoadMore, countBooks, startIndex }) {
   return (
     <div className="search-results">
       { countBooks > startIndex + 30
-        ? <button onClick={loadMore}>Load more</button>
+        ? <button onClick={onLoadMore}>Load more</button>
         : null
       }
       { books.map((book) => (<Card key={book.id} book={book} />)) }
