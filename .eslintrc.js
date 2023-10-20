dcf301601fb63b0bca60147340a8bf186dade1d6
Ivan Nor <ivan.norik@gmail.com>
2023-10-20 22:01:07 +0300

    bad attempt to fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true,
   },
-  extends: ['standard'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
   parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module'
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
   },
-  settings: {
-    react: {
-      version: 'detect' // Указание 'detect' для обнаружения версии React автоматически
-    }
-  }
-}
+};
