d2cf456d0f550ccda4f08145288811bcc01b21da
Ivan Nor <ivan.norik@gmail.com>
2023-10-22 21:15:34 +0300

    fix import paths

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux'; // Импортируйте Provider
-import store from './store'; // Импортируйте ваш Redux store
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    {' '}
-    {/* Оберните компонент App в Provider */}
     <React.StrictMode>
       <App />
     </React.StrictMode>
